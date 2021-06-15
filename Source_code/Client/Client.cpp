22b84be4cc6526c0ce1fe163b8296d21e6e4ba07
= <=>
2021-06-15 16:16:16 +0700

    up code

--- Source_code/Client/Client.cpp
+++ Source_code/Client/Client.cpp
@@ -11,27 +11,46 @@
 #define BUFF_SIZE  2048
 #define MSG_EMPTY 0
 
+#define SIGNUP "SIGNUP"
+#define LOGIN  "LOGIN"
+#define LOGOUT "LOGOUT"
+#define START "START"
+#define QUESTION "QUESTION"
+#define ANSWER "ANSWER"
+#define RESULT "RESULT"
+#define ASSIST "ASSIST"
+#define QUIT "QUIT"
+#define VICTORY "VICTORY"
+#define SERVER_ERROR "10"
+#define BAD_REQUEST "20"
+#define CANNOT_USE_ASSIST "21"
+#define CANNOT_START_GAME "22"
+#define CANNOT_ANSWER "23"
+#define CANNOT_QUIT "24"
+#define SUCCESS "30"
+
 #pragma comment (lib, "Ws2_32.lib")
 
 using namespace std;
 
+string request, response;
+
 string recv(SOCKET &currSoc);
 void send(SOCKET &currSoc, char req[]);
-void game();
 void menu();
+void showHD();
 void startGame();
 void playGame();
-void signUp();
-void logIn();
-void showHD();
-void sign_up();
-void log_in();
-void start_game();
-void answer();
-void assist();
-void quit();
+void showQues();
+string signUp();
+string logIn();
+string logOut();
+string start();
+string answer();
+string assist();
+string quit();
 void solveRequest();
-void solveResponse();
+void solveResponse(char *);
 int main(int argc, char* argv[])
 {
 	// Step 1: Inittiate WinSock
@@ -64,12 +83,22 @@ int main(int argc, char* argv[])
 	printf("Connected server!\n");
 
 	//Step 5: Communicate with server
-	string buff;
-	string res;
 	int ret, messageLen;
-	menu();
 	while (1) {
-
+		menu();
+		messageLen = request.size();
+		if (messageLen == 0) {
+			throw MSG_EMPTY;
+			break;
+		}
+		send(client, &request[0]);
+		request = "";
+		// receive message
+		response = recv(client);
+		response[response.size() - 2] = 0;
+		solveResponse(&response[0]);
+		request = "";
+		response = "";
 	}
 	//Step 6: Close socket
 	closesocket(client);
@@ -176,21 +205,16 @@ void startGame() {
 			cout << " Chon khong hop le vui long chon lai \n";
 	} while (select<1 || select>3);
 	system("cls");
-	if (select == 1) signUp();
+	if (select == 1) request = signUp();
 	else if (select == 2) {
-		logIn();
+		request = logIn();
 	}
 	else if (select == 3) {
 		Sleep(200);
 		menu();
 	}
 }
-void signUp() {
-	//
-}
-void logIn() {
-	//
-}
+
 void playGame() {
 	VeKhung baoNgoai = VeKhung(28, 5, 50, 25, "");
 	VeKhung batDau = VeKhung(46, 8, 27, 5, "        1. Choi ngay");
@@ -200,49 +224,97 @@ void playGame() {
 	batDau.Ve();
 	thoat.Ve();
 	cout << "\r\n\r\n\r\n\r\n\r\n\r\n\r\n";
-	int chon;
+	int select;
 	do
 	{
 		cout << "\nChon muc muon chon: ";
-		cin >> chon;
-		if (chon == 1)
+		cin >> select;
+		if (select == 1)
 			system("cls");
-		if (chon < 1 || chon>3)
+		if (select < 1 || select>3)
 			cout << " Chon khong hop le vui long chon lai \n";
-	} while (chon<1 || chon>3);
+	} while (select<1 || select>2);
+	system("cls");
+	if (select == 1) request = start();
+	else if (select == 2) {
+		Sleep(200);
+		startGame();
+	}
+}
+void showQues() {
+
 }
 void showHD() {
 	cout << "1.  You can play as an entire class or in two groups.\n";
 	cout << "If you play as an entire class, it is suggested that\n";
 	cout << "you call on different students to answer questions.\n";
-	cout <<	"If you play as two teams, you will need to play two games.\n";
-	cout <<	"You will need to appoint a spokesperson for each team.\n";
-	cout <<	"I have also played with two teams, alternating questions\n";
-	cout <<	"between the two and keeping score.\n";
-	cout <<	"2.  Once the game has loaded, and is in slide show view,\n";
-	cout <<	"click on the $100 question.This game is very easy\n";
-	cout <<	"to use since many of the directions are on each slide.\n";
-	cout <<	"If a student would like to use 50 / 50, phone a friend\n";
-	cout <<	"or ask the audience, click on the appropriate button.\n";
-	cout <<	"Then follow the slides.\n";
-	cout <<	"3. Directions for viewing the previous slide are given\n";
-	cout <<	"on each slide.\n";
-	cout <<	"4.  The game is over when a question is missed.1.You can play as an entire class or in two groups.\n";
-	cout <<	"If you play as an entire class, it is suggested that\n";
-	cout <<	"you call on different students to answer questions.\n";
-	cout <<	"If you play as two teams, you will need to play two games.\n";
-	cout <<	"You will need to appoint a spokesperson for each team.\n";
-	cout <<	"I have also played with two teams, alternating questions\n";
-	cout <<	"between the two and keeping score.\n";
-	cout <<	"2.  Once the game has loaded, and is in slide show view,\n";
-	cout <<	"click on the $100 question.This game is very easy\n";
-	cout <<	"to use since many of the directions are on each slide.\n";
-	cout <<	"If a student would like to use 50 / 50, phone a friend\n";
-	cout <<	"or ask the audience, click on the appropriate button.\n";
-	cout <<	"Then follow the slides.\n";
-	cout <<	"3. Directions for viewing the previous slide are given\n";
-	cout <<	"on each slide.\n";
-	cout <<	"4.  The game is over when a question is missed.\n";
+	cout << "If you play as two teams, you will need to play two games.\n";
+	cout << "You will need to appoint a spokesperson for each team.\n";
+	cout << "I have also played with two teams, alternating questions\n";
+	cout << "between the two and keeping score.\n";
+	cout << "2.  Once the game has loaded, and is in slide show view,\n";
+	cout << "click on the $100 question.This game is very easy\n";
+	cout << "to use since many of the directions are on each slide.\n";
+	cout << "If a student would like to use 50 / 50, phone a friend\n";
+	cout << "or ask the audience, click on the appropriate button.\n";
+	cout << "Then follow the slides.\n";
+	cout << "3. Directions for viewing the previous slide are given\n";
+	cout << "on each slide.\n";
+	cout << "4.  The game is over when a question is missed.1.You can play as an entire class or in two groups.\n";
+	cout << "If you play as an entire class, it is suggested that\n";
+	cout << "you call on different students to answer questions.\n";
+	cout << "If you play as two teams, you will need to play two games.\n";
+	cout << "You will need to appoint a spokesperson for each team.\n";
+	cout << "I have also played with two teams, alternating questions\n";
+	cout << "between the two and keeping score.\n";
+	cout << "2.  Once the game has loaded, and is in slide show view,\n";
+	cout << "click on the $100 question.This game is very easy\n";
+	cout << "to use since many of the directions are on each slide.\n";
+	cout << "If a student would like to use 50 / 50, phone a friend\n";
+	cout << "or ask the audience, click on the appropriate button.\n";
+	cout << "Then follow the slides.\n";
+	cout << "3. Directions for viewing the previous slide are given\n";
+	cout << "on each slide.\n";
+	cout << "4.  The game is over when a question is missed.\n";
 	cout << "\n Enter 1 to back menu";
 	cout << "\n Enter anything to quit program\n";
+}
+string signUp() {
+	cin >> request;
+	// append request
+	return request;
+}
+string logIn() {
+	cin >> request;
+	// append request
+	return request;
+}
+string logOut() {
+	// append request
+	return request;
+}
+
+string start() {
+	// append request
+	return request;
+}
+string answer() {
+	cin >> request;
+	// append request
+	return request;
+}
+string assist() {
+	cin >> request;
+	// append request
+	return request;
+}
+string quit() {
+	// append request
+	return request;
+}
+void solveRequest() {
+
+}
+void solveResponse(char *str) {
+	//show message from server
 }
--- Source_code/Server/Server.cpp
+++ Source_code/Server/Server.cpp
@@ -14,10 +14,29 @@
 #define PORT 6000
 #define SERVER_ADDR "127.0.0.1"
 #define BUFF_SIZE 2048
+#define CLIENT_DISCONNECT 0
+
+#define SIGNUP "SIGNUP"
+#define LOGIN  "LOGIN"
+#define LOGOUT "LOGOUT"
+#define START "START"
+#define QUESTION "QUESTION"
+#define ANSWER "ANSWER"
+#define RESULT "RESULT"
+#define ASSIST "ASSIST"
+#define QUIT "QUIT"
+#define VICTORY "VICTORY"
+#define SERVER_ERROR "10"
+#define BAD_REQUEST "20"
+#define CANNOT_USE_ASSIST "21"
+#define CANNOT_START_GAME "22"
+#define CANNOT_ANSWER "23"
+#define CANNOT_QUIT "24"
+#define SUCCESS "30"
+
 using namespace std;
 
-struct ClientInfo
-{
+struct ClientInfo {
 	SOCKET socket;
 	sockaddr_in clientAddr;// address of client
 	string userClient; // user client logined
@@ -26,25 +45,26 @@ struct ClientInfo
 	Question listQues[15];
 	int score;
 	Assist assist;
-
 };
-// struct
+
 // function prototype
 string recv(SOCKET &currSoc);
 void send(SOCKET &currSoc, char req[]);
-void solveRequest();
-void sign_up();
-void log_in();
-void start_game();// solve request START of client
-void answer();
-void assist();
-void quit();
+string solveRequest(ClientInfo*, char[]);
+string signUp(ClientInfo*, char*);
+string logIn(ClientInfo*, char*);
+string logOut(ClientInfo*);
+string start(ClientInfo*, char*);
+string answer(ClientInfo*, char*);
+string assist(ClientInfo*, char*);
+string quit(ClientInfo*, char*);
+void communicating(ClientInfo *);
 
 int main(int argc, char* argv[])
 {
 	DWORD		nEvents = 0;
 	DWORD		index;
-	SOCKET		socks[WSA_MAXIMUM_WAIT_EVENTS];
+	ClientInfo* 	clientInfos[WSA_MAXIMUM_WAIT_EVENTS];
 	WSAEVENT	events[WSA_MAXIMUM_WAIT_EVENTS];
 	WSANETWORKEVENTS sockEvent;
 
@@ -66,13 +86,16 @@ int main(int argc, char* argv[])
 	serverAddr.sin_port = htons(PORT);
 	inet_pton(AF_INET, SERVER_ADDR, &serverAddr.sin_addr);
 
-	socks[0] = listenSock;
+	ClientInfo* clientInfo = new ClientInfo;
+	memset(clientInfo, 0, sizeof(*clientInfo));
+	clientInfo->socket = listenSock;
+	clientInfos[0] = clientInfo;
 	events[0] = WSACreateEvent(); //create new events
 	nEvents++;
 
 	// Associate event types FD_ACCEPT and FD_CLOSE
 	// with the listening socket and newEvent   
-	WSAEventSelect(socks[0], events[0], FD_ACCEPT | FD_CLOSE);
+	WSAEventSelect(clientInfos[0]->socket, events[0], FD_ACCEPT | FD_CLOSE);
 
 
 	if (bind(listenSock, (sockaddr *)&serverAddr, sizeof(serverAddr)))
@@ -88,15 +111,13 @@ int main(int argc, char* argv[])
 	}
 
 	printf("Server started!\n");
-
 	char sendBuff[BUFF_SIZE], recvBuff[BUFF_SIZE];
 	SOCKET connSock;
 	sockaddr_in clientAddr;
 	int clientAddrLen = sizeof(clientAddr);
-	int ret = 0, i;
-
+	int ret, i;
 	for (i = 1; i < WSA_MAXIMUM_WAIT_EVENTS; i++) {
-		socks[i] = 0;
+		clientInfos[i] = 0;
 	}
 	while (1) {
 		//wait for network events on all socket
@@ -107,7 +128,7 @@ int main(int argc, char* argv[])
 		}
 
 		index = index - WSA_WAIT_EVENT_0;
-		WSAEnumNetworkEvents(socks[index], events[index], &sockEvent);
+		WSAEnumNetworkEvents(clientInfos[index]->socket, events[index], &sockEvent);
 
 		if (sockEvent.lNetworkEvents & FD_ACCEPT) {
 			if (sockEvent.iErrorCode[FD_ACCEPT_BIT] != 0) {
@@ -115,11 +136,10 @@ int main(int argc, char* argv[])
 				break;
 			}
 
-			if ((connSock = accept(socks[index], (sockaddr *)&clientAddr, &clientAddrLen)) == SOCKET_ERROR) {
+			if ((connSock = accept(clientInfos[index]->socket, (sockaddr *)&clientAddr, &clientAddrLen)) == SOCKET_ERROR) {
 				printf("Error %d: Cannot permit incoming connection.\n", WSAGetLastError());
 				break;
 			}
-
 			//Add new socket into socks array
 			int i;
 			if (nEvents == WSA_MAXIMUM_WAIT_EVENTS) {
@@ -128,10 +148,15 @@ int main(int argc, char* argv[])
 			}
 			else
 				for (i = 1; i < WSA_MAXIMUM_WAIT_EVENTS; i++)
-					if (socks[i] == 0) {
-						socks[i] = connSock;
+					if (clientInfos[i] == 0) {
+						ClientInfo* clientInfo = new ClientInfo;
+						memset(clientInfo, 0, sizeof(*clientInfo));
+						clientInfo->socket = connSock;
+						clientInfo->clientAddr = clientAddr;
+
+						clientInfos[i] = clientInfo;
 						events[i] = WSACreateEvent();
-						WSAEventSelect(socks[i], events[i], FD_READ | FD_CLOSE);
+						WSAEventSelect(clientInfos[i]->socket, events[i], FD_READ | FD_CLOSE);
 						nEvents++;
 						break;
 					}
@@ -146,37 +171,34 @@ int main(int argc, char* argv[])
 				printf("FD_READ failed with error %d\n", sockEvent.iErrorCode[FD_READ_BIT]);
 				break;
 			}
-
-			//ret = Receive(socks[index], recvBuff, BUFF_SIZE, 0);
-
-			//Release socket and event if an error occurs
-			if (ret <= 0) {
-				closesocket(socks[index]);
-				WSACloseEvent(events[index]);
-				socks[index] = socks[nEvents-1];
-				socks[nEvents - 1] = 0;
-				events[index] = events[nEvents - 1];
-				events[nEvents - 1] = 0;
-				nEvents--;
-			}
-			else {									//echo to client
-				memcpy(sendBuff, recvBuff, ret);
-				//Send(socks[index], sendBuff, ret, 0);
-
-				//reset event
+			try {
+				communicating(clientInfos[index]);
 				WSAResetEvent(events[index]);
 			}
+			catch (int err) {
+				if (err == SOCKET_ERROR || err == CLIENT_DISCONNECT) {
+					cout << "Client disconnect!\n";
+					//string code = quit(clientInfo[index]);
+					closesocket(clientInfos[index]->socket);
+					WSACloseEvent(events[index]);
+					clientInfos[index] = clientInfos[nEvents - 1];
+					clientInfos[nEvents - 1] = 0;
+					events[index] = events[nEvents - 1];
+					events[nEvents - 1] = 0;
+					nEvents--;
+				}
+			}
 		}
-
 		if (sockEvent.lNetworkEvents & FD_CLOSE) {
 			if (sockEvent.iErrorCode[FD_CLOSE_BIT] != 0) {
 				printf("FD_CLOSE failed with error %d\n", sockEvent.iErrorCode[FD_CLOSE_BIT]);
 			}
 			//Release socket and event
-			closesocket(socks[index]);
+			//string code = quit(clientInfos[index]);
+			closesocket(clientInfos[index]->socket);
 			WSACloseEvent(events[index]);
-			socks[index] = socks[nEvents - 1];
-			socks[nEvents - 1] = 0;
+			clientInfos[index] = clientInfos[nEvents - 1];
+			clientInfos[nEvents - 1] = 0;
 			events[index] = events[nEvents - 1];
 			events[nEvents - 1] = 0;
 			nEvents--;
@@ -221,4 +243,90 @@ void send(SOCKET &currSoc, char req[]) {
 		rLeft -= ret;
 		index += ret;
 	}
+}
+
+string solveRequest(ClientInfo *clientInfo, char str[]) {
+	string header, body;
+	for (int i = 0; i < 4; i++) header.push_back(str[i]);
+	for (int i = 5; i < strlen(str); i++) body.push_back(str[i]);
+	string response;
+	/*if (strcmp(&header[0], USER) == 0) {
+		response = login(clientInfo, &body[0]);
+	}
+	else if (strcmp(&header[0], POST) == 0) {
+		response = postMess(loginStruct, &body[0]);
+	}
+	else if (strcmp(&header[0], QUIT) == 0) {
+		response = quit(loginStruct);
+	}
+	else response = UNFORMAT_REQUEST;*/
+	return response;
+}
+
+vector<string> splitRequest(string s) {
+	vector<string> res;
+	string delimiter = "\r\n";
+	size_t pos = 0;
+	string token;
+	while ((pos = s.find(delimiter)) != string::npos) {
+		token = s.substr(0, pos);
+		res.push_back(token);
+		s.erase(0, pos + delimiter.length());
+	}
+	if (s.length()>0) res.push_back(s);
+	return res;
+}
+
+void communicating(ClientInfo *clientInfo) {
+	// receive message from client
+	string buff;
+	string res;
+	buff = recv(clientInfo->socket);
+	if (buff.size() == 0) {
+		//string tmp = quit(clientInfo);
+		return;
+	}
+
+	buff[buff.size() - 2] = 0;
+	// copy message without "\r\n"
+	string tmp;
+	for (int i = 0; i < buff.size() - 2; i++) tmp.push_back(buff[i]);
+	vector<string> reqs = splitRequest(buff);
+	// solve each string of vector string request
+	for (int i = 0; i < reqs.size(); i++) {
+		string req = reqs[i];
+		res = solveRequest(clientInfo, &req[0]);
+		res.append("\r\n");
+		// send response after append "\r\n" to client
+		send(clientInfo->socket, &res[0]);
+	}
+}
+
+string signUp(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
+}
+string logIn(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
+}
+string logOut(ClientInfo* clientInfo) {
+	string response;
+	return response;
+}
+string start(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
+}
+string answer(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
+}
+string assist(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
+}
+string quit(ClientInfo* clientInfo, char* body) {
+	string response;
+	return response;
 }
